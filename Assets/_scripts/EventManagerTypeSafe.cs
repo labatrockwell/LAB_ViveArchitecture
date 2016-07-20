143f33f4d56a697f9f47fef7bf72816455085ebe
David Tracy <dtracy@rockwellgroup.com>
2016-07-20 14:57:22 -0400

    first commit

---
+++ Assets/_scripts/EventManagerTypeSafe.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+// Based on Type-Safe Event System by Will Miller http://www.willrmiller.com/?p=87
+
+// Attach this script to an empty GameObject - name it something like EventManager
+
+//=========== EVENTS - Move this to a separate script ===============
+// These are GameEvent classes or Types of events that will be stored in a dictionary with associated delegate functions
+// Delegates are essentially references to a method somewhere else
+
+//The empty GameEvent class - this will be extended by other the classes that follow
+public class GameEvent {}
+
+//Some examples: 
+public class ChangeOfTimeEvent : GameEvent
+{
+	public int id{ get; private set;}
+	public ChangeOfTimeEvent(int _id){
+		this.id = _id;
+	}
+}
+
+public class FadeEvent : GameEvent
+{
+	public int id{ get; private set;}
+	public FadeEvent(int _id){
+		this.id = _id;
+	}
+}
+
+public class HumanFade : GameEvent
+{
+	public int id{ get; private set;}
+	public HumanFade(int _id){
+		this.id = _id;
+	}
+}
+
+
+public class TeleportEvent : GameEvent
+{
+	public int id{ get; private set;}
+	public TeleportEvent(int _id){
+		this.id = _id;
+	}
+}
+
+//===================================================================
+
+
+public class EventManagerTypeSafe : MonoBehaviour 
+{
+	// declare the Event Manager
+	private static EventManagerTypeSafe eventManager;
+
+	//The dictionary that holds all of our events
+	private Dictionary<System.Type, System.Delegate> eventDictionary = new Dictionary<System.Type, System.Delegate>();
+
+	//declare the delgate with a specific template type and an event argument called 'e'
+	public delegate void EventDelegate<T>(T e) where T : GameEvent;
+
+	//constructor
+	public static EventManagerTypeSafe instance
+	{
+		get
+		{
+			if (!eventManager)
+			{
+				eventManager = FindObjectOfType (typeof (EventManagerTypeSafe)) as EventManagerTypeSafe;
+				if (!eventManager)
+				{
+					Debug.LogError ("There needs to be one active EventManager script on a GameObject in your scene.");
+				}
+				else
+				{
+					eventManager.Init (); 
+				}
+			}
+			return eventManager;
+		}
+	}
+
+	public void Init()
+	{
+		if (eventDictionary == null)
+		{
+			eventDictionary = new Dictionary<System.Type, System.Delegate>();
+		}
+	}
+
+
+	public void AddListener<T> (EventDelegate<T> _delegate) where T : GameEvent
+	{
+		if (eventDictionary.ContainsKey (typeof(T))) {
+			System.Delegate tempDel = eventDictionary [typeof(T)];
+			eventDictionary [typeof(T)] = System.Delegate.Combine (tempDel, _delegate);		
+		} else {
+			eventDictionary [typeof(T)] = _delegate;
+		}
+	}		
+
+	public void RemoveListener<T> (EventDelegate<T> _delegate) where T : GameEvent
+	{
+		if (eventDictionary.ContainsKey(typeof(T)))
+		{
+			var currentDel = System.Delegate.Remove(eventDictionary[typeof(T)], _delegate);
+			if (currentDel == null)
+			{
+				eventDictionary.Remove(typeof(T));
+			}
+			else
+			{
+				eventDictionary[typeof(T)] = currentDel;
+			}
+		}
+	}
+
+	public void TriggerEvent(GameEvent e)
+	{
+		if (e == null)
+		{
+			Debug.Log("Invalid event argument: " + e.GetType().ToString());
+			return;
+		}
+
+		if (eventDictionary.ContainsKey(e.GetType()))
+		{
+			eventDictionary[e.GetType()].DynamicInvoke(e);
+		}		
+		
+	}
+}
---
+++ Assets/_scripts/EventRegister.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Based on Type-Safe Event System by Will Miller http://www.willrmiller.com/?p=87
+
+// Attach this script to a GameObject that will respond to GameEvents
+// The class methods are project specific, but this will outline how to use a delegate funciton with parameters
+
+// The events can be called from another object by calling:
+// EventManagerTypeSafe.instance.TriggerEvent(new ReticleEndEvent());
+
+public class EventRegister : MonoBehaviour {
+	
+	void Start(){
+
+	}
+
+	void OnEnable()
+	{
+		EventManagerTypeSafe.instance.AddListener<ChangeOfTimeEvent> (OnChangeTime);
+	}
+
+	void OnDisable()
+	{
+		//When the object is disabled, tell the Event Manager to remove it to the Event Dictionary
+		EventManagerTypeSafe.instance.RemoveListener<ChangeOfTimeEvent> (OnChangeTime);
+	}
+
+	void OnChangeTime(ChangeOfTimeEvent _e){
+		print("Change of time event!");
+		//Do stuff to the audio emitter
+	}
+
+}
